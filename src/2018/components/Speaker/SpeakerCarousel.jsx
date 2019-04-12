d5fcaa0bc2a34272a1223cb38817354d9d5215c9
JacobDFrank <frankjacob42@gmail.com>
2019-04-12 16:15:58 -0400

    finished speaker carousel

--- src/2018/components/Speaker/SpeakerCarousel.jsx
+++ src/2018/components/Speaker/SpeakerCarousel.jsx
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import SpeakerPhoto from './SpeakerPhoto.jsx';
-import data from '../../assets/speakers.json';
 import classnames from 'classnames';
 import styles from '../../styles/components/speakers/speakerCarousel.module.scss';
 import DragScroll from 'react-dragscroll';
 
-class SpeakerCarousel extends React.Component {
-
-  createPerson = (person) => {
-    return (
-      <div className={classnames(styles.speakerCard, styles.speaker_lineup__padding)}
-        key={person.headshot}>
-        <SpeakerPhoto firstName={person.firstName} lastName={person.lastName} job={person.job} headshot={person.headshot} />
-      </div>
-    );
-  };
-
-  createPeople = (people) => {
-    return people.map(this.createPerson);
-  };
+export default class SpeakerCarousel extends Component {
 
   render() {
     return (
       <div>
         <DragScroll height={'unset'} width={'unset'} className={classnames(styles.speaker_lineup__Carousel, 'flex')}>
-          <div className={classnames('flex')}>
-            {this.createPeople(data.speakers)}
-          </div>
+          <StaticQuery
+            query={SPEAKERSCAROUSEL_2018_QUERY}
+            render={({ allMarkdownRemark }) =>
+              (
+                <div className={classnames('flex')}>
+                  {
+                    allMarkdownRemark.edges.map(speaker => (
+                      <div className={classnames(styles.speakerCard, styles.speaker_lineup__padding)}
+                        key={speaker.node.frontmatter.name}>
+                        <SpeakerPhoto
+                          name={speaker.node.frontmatter.name}
+                          job={speaker.node.frontmatter.job}
+                          bio={speaker.node.frontmatter.bio}
+                          webpage={speaker.node.frontmatter.webpage}
+                          headshot={speaker.node.frontmatter.headshot}
+                        />
+                      </div>
+                    ))
+                  }
+                </div>
+              )
+            }
+          />
         </DragScroll>
       </div>
     );
   }
 }
 
-export default SpeakerCarousel;
+const SPEAKERSCAROUSEL_2018_QUERY = graphql`
+  query speakersCarousel2018 {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/2018/speakers/" } }) {
+      edges {
+        node {
+          frontmatter {
+            name
+            headshot
+            job
+            webpage
+            bio
+          }
+        }
+      }
+    }
+  }`;
--- src/2018/components/Speaker/SpeakerPhoto.jsx
+++ src/2018/components/Speaker/SpeakerPhoto.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classnames from 'classnames';
 import styles from '../../styles/components/speakers/speakerPhoto.module.scss';
 
-let SpeakerPhoto = function statelessFunctionComponentClass(props) {
-  // let imageURL = './assets/speaker-headshots/' + 'arnaud_tanielian.jpg';
-  // let imageURLPrefix = './assets/speaker-headshots/';
-  let imageURLPrefix ='https://thoughtatwork.cias.rit.edu/assets/graphics/2018-imagery/speaker-headshots/';
+export default class SpeakerPhoto extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <img className={classnames(styles.headshotContainer)}
-        src={imageURLPrefix + props.headshot
-          + '.jpg'}
-      />
-      <h2 className={classnames(styles.speakerCard_text__spacing)}>
-        {props.firstName + ' ' + props.lastName}
-      </h2>
-      <p>{props.job}</p>
-    </div>
-  );
-};
+    this.state = {
+    };
+  }
 
-export default SpeakerPhoto;
+  render() {
+    const props = this.props;
+
+    return (
+      <div>
+        <img className={classnames(styles.headshotContainer)}
+          src={'../../' + props.headshot}
+        />
+        <h2 className={classnames(styles.speakerCard_text__spacing)}>
+          {props.name}
+        </h2>
+        <p>{props.job}</p>
+      </div>
+    );
+  }
+}
